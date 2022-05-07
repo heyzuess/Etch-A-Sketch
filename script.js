43198aceb343668b16a490be7b6d9534260ed1da
Jesse Iberri <jesseiberri@gmail.com>
2022-05-06 19:37:15 -0700

    Added basic site layout + Logic

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ class Grid {
         this.width = width;
         this.height = height;
         this.element = document.getElementById('grid');
-        this.class = 'grid-tile';
+        this.class = 'grid-obj';
     }
 
     clearAll () {
@@ -14,6 +14,31 @@ class Grid {
             console.log(tile);
         }
     }
+
+    buildGrid () {
+        for (let i = 0; i < this.width; i++) {
+            let col = document.createElement('div');
+            col.classList.add(this.class);
+            col.classList.add('container');
+            col.classList.add('grid-col');
+        
+            for (let j = 0; j < this.height; j++) {
+                let element = document.createElement('div');
+                element.classList.add(this.class);
+                element.classList.add('grid-tile');
+                let widthStyle = `${this.width}px`;
+                let heightStyle = `${this.height}px`;
+                element.style.padding = `${widthStyle} ${heightStyle} ${widthStyle} ${heightStyle}`;
+        
+                //test
+                //element.innerHTML = `Row: ${i}, Col: ${j}`;
+        
+                col.append(element);
+            }
+        
+            this.element.append(col);
+        }
+    }
 }
 
 let gridWidth = 10;
@@ -21,9 +46,4 @@ let gridHeight = 10;
 let grid = new Grid(gridWidth, gridHeight);
 
 grid.clearAll();
-
-for (let i = 0; i < grid.width; i++) {
-    let element = document.createElement('div');
-    element.classList.add(grid.class);
-    element.innerHTML = i.toString();
-}
+grid.buildGrid();
